580b69c995bb32790f4d2a953d041d90e7fb65f9
Josiah Hunsinger <josiah.hunsinger@logentix.com>
2017-08-19 21:46:18 +0000

    speaker playing at prompt of button

--- raspberry/src/main.py
+++ raspberry/src/main.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 import serial
 import struct
+import subprocess
+import signal
 import os
+
+playFile = "data/rickastley.mp3"
+playProc = None
+def killPlay():
+    global playProc
+    if playProc is not None:
+        pid = playProc.pid
+        os.kill(pid, signal.SIGINT)
+        print("Kill Music")
+    playProc=None
 
 with serial.Serial('/dev/ttyUSB0', 19200, timeout=100) as ser:
     last_val=None
@@ -10,6 +22,14 @@ with serial.Serial('/dev/ttyUSB0', 19200, timeout=100) as ser:
         val = struct.unpack('b', b)[0]
         if last_val is not None and val != last_val:
             if val: # This means the lights just turned on
+                print("Lights turned ON")
+                killPlay()    
+                playProc = subprocess.Popen(["mpg123", "-f", "20000", playFile], shell=False)
+            else:
+                print("Lights turned OFF")
+                killPlay()
+
+        last_val = val
 
 
         # print(val)
